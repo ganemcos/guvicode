3ec2a5fb61a42c62ac0cab447832f0b88137171b
D Sai Ganesh <ganemcos@gmail.com>
2019-07-04 03:16:07 +0530

    12.py -- (update) simplyfied

--- Beginner/set-2/12.py
+++ Beginner/set-2/12.py
@@ -1,18 +1,6 @@
-import math as m
 s = list(input())
-c = 0 
-for i in range(m.floor(len(s)/2)):
-    if s[i] != s[len(s)-1-i]:
-        # print("no")
-        # break
-        continue
-    else:
-        c+=2
-
-# print("yes" if c+1 == len(s) else "no")
-
-# update prev code returns eror if the len(s) is even
-if len(s)%2 != 0:
-    print("yes" if c+1 == len(s) else "no")
+rs = list(reversed(s))
+if s == rs:
+    print("yes")
 else:
-    print("yes" if c == len(s) else "no")
+    print("no")
