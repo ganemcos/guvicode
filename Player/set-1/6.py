651ab14ae5ba662a955f36004a0eb6f2cd705ed7
D Sai Ganesh <ganemcos@gmail.com>
2019-07-05 00:23:22 +0530

    1.py -- common prefix of the string

---
+++ Player/set-1/6.py
@@ -0,0 +1,24 @@
+# switch = 0
+
+# def fswitch():
+#     if switch:
+#         switch = 0
+#     else:
+#         switch = 1
+
+
+# l1 , l2 = list(input().split())
+# bli,bl2 = [],[]
+# for i in range(len(l1)):
+#     if i == 0:
+#         bli.append(switch)
+#         break
+#     # if switch:
+#     #     bli.append(switch)
+#     if i>0 and l[i]!= l[i-1]:
+#         switch()
+#     elif l[i] == l[i-1]:
+#         bli.append(switch)
+        
+    
+# print(l1,bli)
---
+++ Pro/set-1/1.py
@@ -0,0 +1,28 @@
+'''passing small len lists'''
+def get_comon_prefix_len(len,list1,list2):
+    common_len = 0
+    for k in range(len):
+        if list1[k]==list2[k]:
+            common_len+=1
+        else:
+            break
+    return common_len
+
+i = int(input())
+l,cl = [],[]
+for i in range(i):
+    l.append(list(input()))
+# print(len(l))
+for i in range(len(l)):
+    if i != len(l)-1:   
+        # print("i count:",i)
+        list1,list2 = l[i],l[i+1]
+        list1_len , list2_len = len(list1),len(list2)
+        if list1_len < list2_len:
+            common_len = get_comon_prefix_len(list1_len,list1,list2)
+        else:
+            common_len = get_comon_prefix_len(list2_len,list1,list2)
+        # print("common len",common_len," for list ",list1,list2)
+        cl.append(common_len)
+        # print("#"*30)
+print("".join(list1[:common_len]))
