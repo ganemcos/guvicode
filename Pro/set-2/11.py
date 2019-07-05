1f8c7778f5b4ee3485a90d7a645c987ed5917531
D Sai Ganesh <ganemcos@gmail.com>
2019-07-06 03:50:58 +0530

    11.py children

---
+++ Pro/set-2/11.py
@@ -0,0 +1,14 @@
+# from itertools import combinations as c
+# import random as r
+i = int(input())
+# rl = []
+# for i in range(i):
+#     # rl.append(r.randint(1,1000000000000000000))
+#     print(rl.append(r.randint(1,1000000000000000000)))
+# # l = ["ga","ba","sa","ru"]
+# comb = c(rl,2)
+# d = 0
+# for i in comb:
+#     d+=1
+# print(d)
+print(i//2*(i-1))
