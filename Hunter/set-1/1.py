ced66344a8f05df9a151fe27527035daccd0aa2d
D Sai Ganesh <ganemcos@gmail.com>
2019-07-05 17:28:15 +0530

    1.py updated

--- Hunter/set-1/1.py
+++ Hunter/set-1/1.py
@@ -2,6 +2,11 @@ import collections as c
 null = input()
 i = map(int,input().split())
 ocr = c.Counter(i)
+common = []
 for item,count in ocr.items():
     if count >1 :
-        print(item,end= " ")
+        common.append(item)
+if common:
+    print(*common)
+else:
+    print("unique")
