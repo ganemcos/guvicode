cbddc8264ee8f5be3fa67b3489e48768f4800d13
D Sai Ganesh <ganemcos@gmail.com>
2019-07-07 20:13:39 +0530

    15.py

---
+++ Pro/set-2/15.py
@@ -0,0 +1,15 @@
+# null = input()
+# i = sorted(map(int,input().split()),reverse= True)
+# for o in i:
+#     print(o)
+
+null = input()
+l = list(map(int,input().split()))
+bl = list(map(bin,l))
+bls = sorted(bl,reverse = True)
+print(*bls)
+for i in bls:
+    print(int(i,2))
+i = sorted(map(int,input().split()),reverse= True)
+for te in i:
+    print(te)
