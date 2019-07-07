d7c3befa05ae7d2f81d1af9de5fab0d3ba96a67e
D Sai Ganesh <ganemcos@gmail.com>
2019-07-07 20:10:17 +0530

    15.py -- reverse sort of binary

--- Pro/set-2/15.py
+++ Pro/set-2/15.py
@@ -1,4 +1,12 @@
+# null = input()
+# i = sorted(map(int,input().split()),reverse= True)
+# for o in i:
+#     print(o)
+
 null = input()
-i = sorted(map(int,input().split()),reverse= True)
-for o in i:
-    print(o)
+l = list(map(int,input().split()))
+bl = list(map(bin,l))
+bls = sorted(bl,reverse = True)
+print(*bls)
+for i in bls:
+    print(int(i,2))
