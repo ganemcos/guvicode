3ba10614c8e52046ceef2a22827a6c5b64518de2
D Sai Ganesh <ganemcos@gmail.com>
2019-07-11 16:02:28 +0530

    update 22.py

--- Hunter/set-3/22.py
+++ Hunter/set-3/22.py
@@ -2,8 +2,8 @@ i = int(input())
 l = list(map(int,input().split())) 
 for i in range(len(l)):
     prod = 1
-    for j in l:
-        if j != l[i]:
-            prod*= j
+    for j in range(len(l)):
+        if j != i:
+            prod*= l[j]
     print(prod,end = " ")
     
