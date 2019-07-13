9edd7c3ce6266058e435a5553f0169f86620d45c
D Sai Ganesh <ganemcos@gmail.com>
2019-07-14 00:05:52 +0530

    16.py

--- Pro/set-2/16.py
+++ Pro/set-2/16.py
@@ -1,15 +1,21 @@
 null = input()
 l = list(map(int,input().split()))
-choclate =0 
+choclate = 0
+    
 while len(set(l)) != 1:
-    print(len(set(l)))
-    print(l,choclate)
+    # print(len(set(l)))
+    # print(l,choclate)
     for i in range(len(l)):
         if l[i] != 0:
             choclate+=1
             l[i] -=1 
-    l.remove(0)
+    for i in l:
+        try:
+            l.remove(0)
+        except:
+            pass
+if len(set(l)) == 1:
+    choclate += len(list(l))
 print(choclate)
-
 
 #todo
---
+++ Pro/set-4/31.py
@@ -0,0 +1,10 @@
+n,m = map(int,input().split())
+l = []
+v = []
+for i in range(m):
+    t = list(map(1,input().split()))
+    t[0] = []
+    v.append(t[0])
+    l.append(t)
+v = list(set(v))
+t.append()
