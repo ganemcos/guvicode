410c0a88e99308a25285e5a57baf53b62cac1973
D Sai Ganesh <ganemcos@gmail.com>
2019-07-14 01:43:46 +0530

    update 16.py

--- Pro/set-2/16.py
+++ Pro/set-2/16.py
@@ -1,21 +1,10 @@
-null = input()
+n = int(input())
 l = list(map(int,input().split()))
-choclate = 0
-    
-while len(set(l)) != 1:
-    # print(len(set(l)))
-    # print(l,choclate)
-    for i in range(len(l)):
-        if l[i] != 0:
-            choclate+=1
-            l[i] -=1 
-    for i in l:
-        try:
-            l.remove(0)
-        except:
-            pass
-if len(set(l)) == 1:
-    choclate += len(list(l))
-print(choclate)
+p = [1]*n
 
-#todo
+for i in range(n):
+    if i == 0 and l[i] > l[i+1]:
+        p[i] += p[i+1]
+    if i > 0 and l[i] > l[i-1]:
+        p[i] += p[i-1]
+print(sum(p))
